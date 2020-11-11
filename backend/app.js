7c1d5a039de4baa4b5fb74ce7b524edf098ab49c
SarahHayat <saraahyt@gmail.com>
2020-11-11 16:22:50 +0100

    Connection to server and db

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+const authRoutes = require('./routes/user')
+
+mongoose.connect('mongodb+srv://SarahHayat:admin@cluster0.656un.mongodb.net/Cluster0?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.use("/test",(req, res, next) => {
+  res.status(200).json({message : "Requete recu "})
+    next()
+});
+
+app.use("/api/auth", authRoutes )
+
+module.exports = app;
